b79b04c8f9b613447e4678d4ce80cf964b0cbce7
Justin Law <jlawhome08@gmail.com>
2018-11-15 18:56:46 -0800

    remove leftover controller stuff

--- controllers/default.py
+++ controllers/default.py
@@ -8,91 +8,9 @@
 # - download is for downloading files uploaded in the db (does streaming)
 # -------------------------------------------------------------------------
 
-import datetime
-
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
 
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-
-    if session.c is None:
-        session.c = 1
-    else:
-        session.c += 1
-
-    rows = db(db.post.id > 0).select()
-
-    return dict(
-        message=T('Welcome to web2py!'),
-        ctime=datetime.datetime.now().isoformat(),
-        visit_count=session.c,
-        rows=rows,
-    )
-
-
-def add1():
-    """Very simple way to insert records."""
-    logger.info("Request method: %r", request.env.request_method)
-    if request.env.request_method == 'POST':
-        # This is a postback.  Insert the record.
-        logger.info("We are inserting: title: %r content: %r" % (
-            # I could also access the title via request.post_vars.post_title,
-            # but I don't care to differentiate betw post and get variables.
-            request.vars.post_title, request.vars.post_content
-        ))
-        db.post.insert(
-            post_title=request.vars.post_title,
-            post_content=request.vars.post_content
-        )
-        redirect(URL('default', 'index'))
-    else:
-        # This is a GET request.  Returns the form.
-        return dict()
-
-
-def add2():
-    """More sophisticated way, in which we use web2py to come up with the form."""
-    form = SQLFORM.factory(
-        Field('post_title'),
-        Field('post_content', 'text'),
-    )
-    # We can process the form.  This will check that the request is a POST,
-    # and also perform validation, but in this case there is no validation.
-    if form.process().accepted:
-        # We insert the result, as in add1.
-        db.post.insert(
-            post_title=form.vars.post_title,
-            post_content=form.vars.post_content
-        )
-        # And we load default/index via redirect.
-        redirect(URL('default', 'index'))
-    # We ask web2py to lay out the form for us.
-    logger.info("My session is: %r" % session)
-    return dict(form=form)
-
-
-def add3():
-    """More sophisticated way, in which we use web2py to come up with the form."""
-    form = SQLFORM(db.post)
-    # We can process the form.  This will check that the request is a POST,
-    # and also perform validation, but in this case there is no validation.
-    # THIS process() also inserts.
-    if form.process().accepted:
-        # NOT NEEDED We insert the result, as in add1.
-        # db.post.insert(
-        #     post_title=form.vars.post_title,
-        #     post_content=form.vars.post_content
-        # )
-        # And we load default/index via redirect.
-        redirect(URL('default', 'index'))
-    # We ask web2py to lay out the form for us.
-    logger.info("My session is: %r" % session)
-    return dict(form=form)
-
+    return dict()
 
 
 def user():
