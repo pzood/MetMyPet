c4ef20fb7112644e5e1a1b9878cffd287d48005a
Pulkit Sood <pusood@ucsc.edu>
2018-11-28 21:26:41 -0800

    fixed api.py

--- controllers/api.py
+++ controllers/api.py
@@ -1,14 +1,4 @@
 # Here go your api methods.
-<<<<<<< HEAD
-
-def __make_profile():
-	profile_entry = db.profile.insert(
-		contact_info = auth.user_email,
-		city = request.cars.city,
-		)
-	return response.JSON(dict(profile_entry=profile_entry))
-
-=======
 @auth.requires_signature()
 def make_profile():
 	profile_entry = db.profile.insert(
@@ -18,53 +8,29 @@ def make_profile():
 	return response.JSON(dict(profile_entry=profile_entry))
 
 # for sitter form
->>>>>>> 37e25f6f461d4b6700b99e98285124c9dfc07806
 @auth.requires_signature()
 def add_sitter():
 	sitter_id = db.sitter.insert(
-		live = request.vars.live,
-<<<<<<< HEAD
-		description = request.vars.description,
-		)
+	    live = request.vars.live,
+	    description = request.vars.description,
+	)
 	return response.JSON(dict(sitter_id=sitter_id))
 
-=======
-		descirption = request.vars.description,
-	)
-	return respone.JSON(dict(sitter_id=sitter_id))
-
 # for owner form
->>>>>>> 37e25f6f461d4b6700b99e98285124c9dfc07806
 @auth.requires_signature()
 def add_owner():
 	owner_id = db.pet_owner.insert(
-		live = request.vars.live,
-<<<<<<< HEAD
-		description = request.vars.description,
-		)
-	return response.JSON(dict(owner_id = owner_id))
-
-@auth.requires_singature()
-=======
-		descirption = request.vars.description,
+	    live = request.vars.live,
+	    description = request.vars.description,
 	)
-	return response.JSON(dict(owner_id=owner_id))
+	return response.JSON(dict(owner_id = owner_id))
 
 #For adding pets (like replies)
 @auth.requires_signature()
->>>>>>> 37e25f6f461d4b6700b99e98285124c9dfc07806
 def add_pet():
 	pet_entry = db.pet.insert(
-		name = request.vars.name,
-		species = request.vars.species,
-<<<<<<< HEAD
-		description = request.vars.description
-		)
-	return response.JSON(dict(pet_entry = pet_entry))
-
-
-=======
-		descirption = request.vars.description,
+	    name = request.vars.name,
+	    species = request.vars.species,
+	    description = request.vars.description
 	)
-	return response.JSON(dict(pet_entry=pet_entry))
->>>>>>> 37e25f6f461d4b6700b99e98285124c9dfc07806
+	return response.JSON(dict(pet_entry = pet_entry))
