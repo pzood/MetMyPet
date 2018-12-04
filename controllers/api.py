a847705e0b405b8d398ab462fffcb8f2440830b6
Justin Law <jlawhome08@gmail.com>
2018-12-03 18:59:55 -0800

    Get Profile List Works
    Joins auth_user, profiles, and [sitter/owner].

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,31 @@
 # Here go your api methods.
+
+import logging
+
+# set up debug logger
+log = logging.getLogger("oatmeal")
+log.setLevel(logging.DEBUG)
+
+def get_profiles_list():
+    log.debug(get_owners_list())
+    log.debug("\n")
+    log.debug(get_sitters_list())
+    return "ok"
+
+def get_owners_list():
+    rows = db().select(db.auth_user.ALL, db.profile.ALL, db.pet_owner.ALL,
+                       join=[
+                           db.auth_user.on(db.auth_user.id == db.profile.userID),
+                           db.profile.on(db.profile.id == db.pet_owner.profileID)
+                       ])
+
+    return response.json(dict(rows=rows))
+
+def get_sitters_list():
+    rows = db().select(db.auth_user.id, db.profile.userID, db.profile.id, db.sitter.profileID,
+                       join=[
+                           db.auth_user.on(db.auth_user.id == db.profile.userID),
+                           db.profile.on(db.profile.id == db.sitter.profileID)
+                       ])
+
+    return response.json(dict(rows=rows))
