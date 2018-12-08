3178db814b3b1a1fd09a5cb9d1e1c4b8a95f6c27
Justin Law <jlawhome08@gmail.com>
2018-12-07 18:22:47 -0800

    Grabbing Sitters By City Order Works
    nice

--- controllers/api.py
+++ controllers/api.py
@@ -1,36 +1,43 @@
 # Here go your api methods.
 
 import logging
+import urllib, json
 
 # set up debug logger
 log = logging.getLogger("oatmeal")
 log.setLevel(logging.DEBUG)
 
+# grab the list of profiles I need
 def get_profiles_list():
-    rows = get_sitters_list()
-    log.debug(rows)
+    url = "http://getnearbycities.geobytes.com/GetNearbyCities?&radius=100&locationcode=Scotts Valley,%20CA&limit=20"
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+
+    cities = []
+    for city in data:
+        cities.append(city[1])
+
+    log.debug(get_sitters_list(cities))
     return "ok"
 
-def get_owners_list():
-    rows = db().select(db.auth_user.ALL, db.profile.ALL, db.pet_owner.ALL,
-                       join=[
-                           db.auth_user.on(db.auth_user.id == db.profile.userID),
-                           db.profile.on(db.profile.id == db.pet_owner.profileID)
-                       ])
-
-    return response.json(dict(rows=rows))
-
-def get_sitters_list():
+# grab the sitters I need by city distance order
+def get_sitters_list(cities):
     set = db(db.auth_user.id == db.profile.userID)
     set = set(db.profile.id == db.sitter.id)
     rows = []
 
-    temp = set(db.profile.city == 'Santa Cruz')
-    temp.select(db.auth_user.ALL, db.profile.ALL, db.sitter.ALL,)
+    for city in cities:
+        temp = set(db.profile.city == city)
+        temp = temp.select(db.auth_user.ALL, db.profile.ALL, db.sitter.ALL)
 
-    for row in temp :
-        id = row.profile.id
-        row.score = db(db.sitter_review.profileID == id).select(db.sitter_review.rating.avg())
-        rows.append(row)
+        for row in temp :
+            id = row['profile']['id']
+            avgScore = db.sitter_review.rating.avg()
+            row['score'] = db(db.sitter_review.revieweeID == id).select(avgScore).first()[avgScore]
+            rows.append(row)
 
     return response.json(dict(rows=rows))
+
+
+def get_owners_list():
+    return response.json(dict())
