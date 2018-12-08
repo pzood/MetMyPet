82966fa54e10af076a8fbd7d6ac0bea3c7c1e56b
Justin Law <jlawhome08@gmail.com>
2018-12-07 17:02:18 -0800

    Fixed Conflicts
    :y

--- controllers/api.py
+++ controllers/api.py
@@ -7,9 +7,8 @@ log = logging.getLogger("oatmeal")
 log.setLevel(logging.DEBUG)
 
 def get_profiles_list():
-    log.debug(get_owners_list())
-    log.debug("\n")
-    log.debug(get_sitters_list())
+    rows = get_sitters_list()
+    log.debug(rows)
     return "ok"
 
 def get_owners_list():
@@ -22,10 +21,16 @@ def get_owners_list():
     return response.json(dict(rows=rows))
 
 def get_sitters_list():
-    rows = db().select(db.auth_user.id, db.profile.userID, db.profile.id, db.sitter.profileID,
-                       join=[
-                           db.auth_user.on(db.auth_user.id == db.profile.userID),
-                           db.profile.on(db.profile.id == db.sitter.profileID)
-                       ])
+    set = db(db.auth_user.id == db.profile.userID)
+    set = set(db.profile.id == db.sitter.id)
+    rows = []
+
+    temp = set(db.profile.city == 'Santa Cruz')
+    temp.select(db.auth_user.ALL, db.profile.ALL, db.sitter.ALL,)
+
+    for row in temp :
+        id = row.profile.id
+        row.score = db(db.sitter_review.profileID == id).select(db.sitter_review.rating.avg())
+        rows.append(row)
 
     return response.json(dict(rows=rows))
